d459847152b6d4742e65913dfbec5bf7295f3f98
Jonathan <j_passe@live.fr>
2016-06-08 15:32:19 +0100

    chap 21 refactored by using web helpers

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -2,22 +2,14 @@ require 'spec_helper'
 
 feature "fill in players names" do
 	scenario "players enter their names" do
-		visit '/'
-		fill_in :player_1_name, with:  'name1'
-		fill_in :player_2_name, with:  'name2'
-
-		click_button 'Submit'
-
+		sign_in_and_play
 		expect(page).to have_content "name1 vs name2"
 	end
 end
 
 feature "viewing players hit points" do
   scenario "view player 2 hit points" do
-    visit '/'
-    fill_in :player_1_name, with:  'name1'
-    fill_in :player_2_name, with:  'name2'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'name2 has 50 points, name1 has 50 points'
   end
 end
