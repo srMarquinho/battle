fd959ba195dd3708308145d98083483e4dbedd42
Luke <lukeaveil@yahoo.com>
2016-06-09 15:45:18 +0100

    challenge 26, refactored to keep controllers skinny

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,20 @@ require_relative 'player'
 
 class Game
 
+  def initialize(player_1, player_2)
+  	@players = [player_1, player_2]
+  end
+
   def attack(opponent)
     opponent.receive_damage
+  end
+
+  def player_1
+  	@players.first
+  end
+
+  def player_2
+  	@players.last
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require 'game'
 
 describe Game do
 	
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player_1, player_2) }
   
   let(:player_1) { double(:player_1) }
   let(:player_2) { double(:player_2) }
@@ -14,4 +14,32 @@ describe Game do
     end
   end
 
+  describe '#player_1' do
+
+  	it 'retrieves the first player' do
+  		expect(game.player_1).to eq player_1
+  	end
+
+  end
+
+  describe '#player_2' do
+
+  	it 'retrieves the second player' do
+  		expect(game.player_2).to eq player_2
+  	end
+
+  end
+
 end 
+
+
+
+
+
+
+
+
+
+
+
+
